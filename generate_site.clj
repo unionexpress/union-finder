ff449f0825f2e933665e29180fcb06e730e4bcab
Pavlos Melissinos <pavlos@baresquare.com>
2025-06-18 16:25:53 +0300

    Improve metadata

--- generate_site.clj
+++ generate_site.clj
@@ -180,7 +180,7 @@
 <head>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>Εύρεση Σωματείων - Θεσσαλονίκη</title>
+    <title>Βρες το Σωματείο σου</title>
     <style>
         * {
             margin: 0;
